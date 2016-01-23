8fd51c99c139b3e023ba434c67da9e50ae3e2f37
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2016-01-23 14:02:12 +0300

    initial commit

---
+++ js/components/Email.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react'
+
+export class Email extends React.Component {
+  render() {
+    return (
+      // <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+      // "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      // <html xmlns="http://www.w3.org/1999/xhtml">
+      //   <head>
+      //     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      //     <meta name="viewport" content="width=device-width"/>
+      //     <link rel="stylesheet" href="css/vendor/ink.css"/> /* For testing only */
+      //     <style type="text/css">
+      //
+      //       /* Ink styles go here in production */
+      //
+      //     </style>
+      //     <style type="text/css">
+      //       /* Your custom styles go here */
+      //     </style>
+      //   </head>
+      //   <body style="background:#ddd">
+      //     <table class="body" style="background:#ddd"> /* Background goes here too */
+      //       <tr>
+      //         <td class="center" align="center" valign="top">
+      //           <center>
+      //             {this.props.children}
+      //           </center>
+      //         </td>
+      //       </tr>
+      //     </table>
+      //   </body>
+      // </html>
+      <p>
+        This is an email body
+      </p>
+    )
+  }
+}
