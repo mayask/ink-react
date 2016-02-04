fb4e59bb0cc027f717e513558a290d1ed5e11c72
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2016-02-04 22:51:56 +0300

    added row offset support

--- js/components/Row.jsx
+++ js/components/Row.jsx
@@ -11,9 +11,13 @@ export class Row extends InkComponent {
         <tbody>
           <tr>
             {React.Children.map(children, function (child, i) {
+              let offsetClass = {}
+              if (React.isValidElement(child)) {
+                offsetClass[`offset-by-${child.props.offset}`] = child.props.offset
+              }
               var rowChildClass = classnames('wrapper', {
-                'last': (i + 1) === React.Children.count(children)
-              })
+                'last': (i + 1) === React.Children.count(children),
+              }, offsetClass)
               return (
                 <td key={i} className={rowChildClass}>
                   {child}
